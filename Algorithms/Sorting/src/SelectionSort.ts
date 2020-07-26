0776daa3894ca1ce31e3a3ee2cb22f860b515e92
Daniel Goh <gohdaniel15@gmail.com>
2020-07-26 09:54:30 +0800

    Fix sorts

--- Algorithms/Sorting/src/SelectionSort.ts
+++ Algorithms/Sorting/src/SelectionSort.ts
@@ -1,12 +1,16 @@
 // Time complexity: O(n^2)
 // Space complexity: O(1)
 export default function SelectionSort(input: Array<number>): Array<number> {
-  let smallest: number = Number.MIN_VALUE
   for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input.length; j++) {
-      if (input[j] < smallest) smallest = input[j]
-      if (j === input.length - 1) input[0] = smallest
+    let smallest: number = i
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[j] < input[smallest]) {
+        smallest = j
+      }
     }
+    let tempValue = input[i]
+    input[i] = input[smallest]
+    input[smallest] = tempValue
   }
 
   return input
--- Algorithms/Sorting/tests/Sort.test.js
+++ Algorithms/Sorting/tests/Sort.test.js
@@ -4,7 +4,8 @@ import SelectionSort from '../dist/SelectionSort.js'
 
 test.beforeEach(t => {
   const input = [720581, 281166, 502986, 600780, 115587, 111002, 285569, 7163, 130191, 403572, 131179, 397911, 907564, 575761, 95838, 525387, 798957, 246577, 89438, 748819, 870200, 729004, 239881, 59353, 376572, 136308, 549790, 512798, 420776, 369234, 973719, 10841, 933355, 10613, 375010, 741133, 743163, 412440, 225414, 485910, 784186, 846288, 680935, 958468, 784197, 636556, 521797, 34781, 784397, 479065]
-  const result = input.sort()
+  const copiedInput = Array.from(input)
+  const result = copiedInput.sort((a, b) => a - b)
 
   t.context = {
     input,
@@ -14,9 +15,9 @@ test.beforeEach(t => {
 
 test('#BubbleSort', t => {
   const { input, result } = t.context
-  t.is(BubbleSort(input), result)
+  t.deepEqual(BubbleSort(input), result)
 })
 test('#SelectionSort', t => {
   const { input, result } = t.context
-  t.is(SelectionSort(input), result)
+  t.deepEqual(SelectionSort(input), result)
 })
